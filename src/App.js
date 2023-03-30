e35594c1611fdde4f9ae67f5875a1ab88e536e75
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-03-30 16:08:15 -0300

    agrego componentes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hola mundo!</h1>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        h1 > hola mundo! < /h1> <
+        /header> <
+        /div>
+    );
 }
 
 export default App;
