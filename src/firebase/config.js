629097110805aa9edf740a867f666d404d660c37
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-05-19 23:09:17 -0300

    FranciShop 1.1.0 (agrego variables de entorno)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,17 @@
 import firebase from "firebase/app";
 import 'firebase/firestore';
-import firebaseConfig from "../firebaseConfig";
-const config = {
-    apiKey:  firebaseConfig.apiKey,
-    authDomain: firebaseConfig.authDomain,
-    projectId: firebaseConfig.projectId,
-    storageBucket: firebaseConfig.storageBucket,
-    messagingSenderId: firebaseConfig.messagingSenderId,
-    appId: firebaseConfig.appId,
-    measurementId: firebaseConfig.measurementId
-};
 
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 // Initialize Firebase
-const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const getFirestore=()=>{
     return firebase.firestore(app)
