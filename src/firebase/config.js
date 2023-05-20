5e3c6767a6416a984286fada83bae45df3536a8b
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-05-19 21:04:54 -0300

    FrancisShop 1.0.1

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,18 @@
 import firebase from "firebase/app";
 import 'firebase/firestore';
-const firebaseConfig = {
-
+import firebaseConfig from "../firebaseConfig";
+const config = {
+    apiKey:  firebaseConfig.apiKey,
+    authDomain: firebaseConfig.authDomain,
+    projectId: firebaseConfig.projectId,
+    storageBucket: firebaseConfig.storageBucket,
+    messagingSenderId: firebaseConfig.messagingSenderId,
+    appId: firebaseConfig.appId,
+    measurementId: firebaseConfig.measurementId
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(config);
 // const analytics = getAnalytics(app);
 export const getFirestore=()=>{
     return firebase.firestore(app)
